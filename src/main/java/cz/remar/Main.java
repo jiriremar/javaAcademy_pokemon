dd2c5225a72b59d879ba773620df6b84053e06ac
jiriremar <jiriremar@icloud.com>
2024-12-03 05:54:29 +0100

    Add and configure Hikary pool

--- src/main/java/cz/remar/Main.java
+++ src/main/java/cz/remar/Main.java
@@ -6,20 +6,21 @@ public class Main {
     public static void main(String[] args) {
 
         String select = "SELECT * FROM pokemon";
-        String connectionUrl = "jdbc:mysql://localhost:3306/pokemons";
         String catchtable = "";
 
-        try {
-            Connection conn = DriverManager.getConnection(connectionUrl, "root", "password");
-            PreparedStatement ps = conn.prepareStatement(select);
-            ResultSet rs = ps.executeQuery();
+        try (
+                Connection conn = HikaryCPDataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(select);
+                ResultSet rs = ps.executeQuery()
+        ) {
+
 
             while (rs.next()) {
                 String name = rs.getString("pokemon_name");
                 int trainer = rs.getInt("id_trainer");
-                if (trainer > 0){
+                if (trainer > 0) {
                     catchtable = "NE";
-                }else {
+                } else {
                     catchtable = "ANO";
                 }
                 System.out.println("Jméno: " + name + " můžeme ho chytat: " + catchtable);
